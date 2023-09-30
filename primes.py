5cc84d34ed62be5e4e090e4c6d4a5a306614e5c8
Zehra Hasan <zehrahasan157@hotmail.com>
2023-09-30 23:00:41 +0100

    raise ValueError exception number_of_primes is less than 1

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkPrime(num):
+    if num < 2:
+        return False
+    for i in range (2, num):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError(f"number of primes = {number_of_primes}. This should be > 0")
     list = []
+    currentNum = 0
+    while (len(list) < number_of_primes):
+        if checkPrime(currentNum):
+            list.append(currentNum)
+        currentNum += 1
     return list
